dd23faa867556e2297a9d7ccb3f8851947517b2c
cyberpunk1971 <adt71@cox.net>
2018-03-14 22:04:20 -0700

    working on routing

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 'use strict';
 
-require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
@@ -9,14 +8,31 @@ const { router: usersRouter } = require('./users');
 const { router: authRouter, localStrategy, jwtstrategy } = require('./auth');
 mongoose.Promise = global.Promise;
 const { PORT, DATABASE_URL } = require('./config');
-const { User } = require('./models');
 const app = express();
 app.use(morgan('common'));
 app.use(express.static('public'));
 
-app.post('/users', (req, res => {
-  const requiredFields = ['']
-}))
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Header', 'Content-type, Authorization');
+  res.header('Access-Control-Allow-Methods', GET, POST, PUT, PATCH, DELETE);
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
+
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+app.use('/api/users/', usersRouter);
+app.use('/api/auth/', authRouter);
+const jwtAuth = passport.authenticate('jwt', { session: false });
+app.get('/api/protected', jwtAuth, (req, res) => {
+  return res.json({
+
+  });
+});
+
 
 if (require.main === module) {
   app.listen(process.env.PORT || 8081, function() {
