693ffafbce094d7a7af3a66eae49697247211ba5
cyberpunk1971 <adt71@cox.net>
2018-03-15 10:36:04 -0700

    added run/close server functions to server.js

--- server.js
+++ server.js
@@ -29,9 +29,44 @@ app.use('/api/auth/', authRouter);
 const jwtAuth = passport.authenticate('jwt', { session: false });
 app.get('/api/protected', jwtAuth, (req, res) => {
   return res.json({
-
+    data: 'newuser'
   });
 });
+app.use('*', (req, res) => {
+  return res.status(404).json({message: 'Not Found'});
+});
+
+let server;
+
+function runServer(databaseUrl, port =  PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('closing server');
+      if (err) {
+        return reject(err);
+      }
+      resolve();
+    });
+  });
+}
 
 
 if (require.main === module) {
