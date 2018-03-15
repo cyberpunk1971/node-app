88d52c9e2ccbefaf1f1f4e7c1edfed0bb61d6bb7
cyberpunk1971 <adt71@cox.net>
2018-03-15 16:56:51 -0700

    commit to master

---
+++ public/dashboard.js
@@ -0,0 +1,31 @@
+const STATE = {
+  "medications": [
+    {
+      "brand_name": "Tylenol",
+      "dosage_form": "tablet",
+      "generic_name": "acetaminophen",
+      "route": "oral",
+      "substance_name": "acetaminophen"
+    },
+    {
+      "brand_name": "Tylenol",
+      "dosage_form": "tablet",
+      "generic_name": "acetaminophen",
+      "route": "oral",
+      "substance_name": "acetaminophen"
+    }
+    {
+      "brand_name": "Tylenol",
+      "dosage_form": "tablet",
+      "generic_name": "acetaminophen",
+      "route": "oral",
+      "substance_name": "acetaminophen"
+    }
+  ],
+  "userName": {
+    "firstName": "Alex",
+    "lastName": "Thomas"
+  }
+}
+
+$('#js-results').append();
