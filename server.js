c2c7a13ba089fda70a5fe2206b0b7d7a7ea02c55
cyberpunk1971 <adt71@cox.net>
2018-03-15 14:02:23 -0700

    starting over

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8081);
+if (require.main === module) {
+  app.listen(process.env.PORT || 8081, function() {
+    console.info(`Your app is listening on ${this.address().port}`);
+  });
+}
+
+module.exports = app;
