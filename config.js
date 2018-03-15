8dc70543ec42d8a95fe6d44f398655c425648abc
cyberpunk1971 <adt71@cox.net>
2018-03-14 21:31:33 -0700

    deleted .env file, what an idiot.

--- config.js
+++ config.js
@@ -5,4 +5,4 @@ exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL ||
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/node-users-app';
 exports.PORT = process.env.PORT || 8081;
 exports.JWT_SECRET = process.env.JWT_SECRET;
-exports.JWT_EXPIRY= process.env.JWT_EXPIRY;
+exports.JWT_EXPIRY= process.env.JWT_EXPIRY || '24h';
