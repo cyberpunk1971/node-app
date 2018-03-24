e8ff9f4411b7ad772182711eb81a000620aeb264
cyberpunk1971 <adt71@cox.net>
2018-03-23 22:21:35 -0700

    adjusted file tree structure

--- public/index.js
+++ public/index.js
@@ -1,12 +1,13 @@
 function addTooList() {
-  $('body').on('submit', '#js-form', function(event) {
+  $('body').on('submit', '#js-form' function(event) {
     event.preventDefault();
     const registerValues = {
       firstName: $('#js-fname').val(),
       lastName: $('#js-lname').val(),
       userName: $('#js-uname').val(),
       passWord: $('#js-password').val()
+      email: $('#js-email').val();
     };
-  $.ajax('/api/users', )
+  $.ajax('/api/users', registerValues);
   });
 }
