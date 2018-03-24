9bb65770de3927e3d32a8123711c4f2acb4da78e
cyberpunk1971 <adt71@cox.net>
2018-03-24 00:25:52 -0700

    cannot GET when submitting through browser

--- public/index.js
+++ public/index.js
@@ -1,13 +1,17 @@
 function addTooList() {
-  $('body').on('submit', '#js-form' function(event) {
+  $('body').on('submit', '#js-form', function(event) {
     event.preventDefault();
     const registerValues = {
       firstName: $('#js-fname').val(),
       lastName: $('#js-lname').val(),
       userName: $('#js-uname').val(),
       passWord: $('#js-password').val()
-      email: $('#js-email').val();
+      // email: $('#js-email').val();
     };
   $.ajax('/api/users', registerValues);
   });
 }
+
+$('#js-submit').on('click', function() {
+  addTooList();
+});
