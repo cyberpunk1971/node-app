48c24362271c3ee36ce1191cdd668209afedccc2
cyberpunk1971 <adt71@cox.net>
2018-04-07 22:32:57 -0700

    some styling to registration page added

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,45 +1,45 @@
-const STATE = {
-  "searchMeds": [{
-    "brand_name": "",
-    "dosage_form": "",
-    "generic_name": "",
-    "route": "",
-    "substance_name": ""
-  }],
-  "medications": [{
-      "brand_name": "Tylenol",
-      "dosage_form": "tablet",
-      "generic_name": "Acetaminophen",
-      "route": "oral",
-      "substance_name": "Acetaminophen"
-    },
-    {
-      "brand_name": "Prilosec",
-      "dosage_form": "tablet",
-      "generic_name": "Omeprazole",
-      "route": "oral",
-      "substance_name": "Omeprazole"
-    },
-    {
-      "brand_name": "Lasix",
-      "dosage_form": "tablet",
-      "generic_name": "Furosemide",
-      "route": "oral",
-      "substance_name": "Furosemide"
-    },
-    {
-      "brand_name": "Zestril",
-      "dosage_form": "tablet",
-      "generic_name": "Lisinopril",
-      "route": "oral",
-      "substance_name": "Lisinopril"
-    },
-  ],
-  "userName": {
-    "firstName": "Alex",
-    "lastName": "Thomas"
-  }
-}
+// const STATE = {
+//   "searchMeds": [{
+//     "brand_name": "",
+//     "dosage_form": "",
+//     "generic_name": "",
+//     "route": "",
+//     "substance_name": ""
+//   }],
+//   "medications": [{
+//       "brand_name": "Tylenol",
+//       "dosage_form": "tablet",
+//       "generic_name": "Acetaminophen",
+//       "route": "oral",
+//       "substance_name": "Acetaminophen"
+//     },
+//     {
+//       "brand_name": "Prilosec",
+//       "dosage_form": "tablet",
+//       "generic_name": "Omeprazole",
+//       "route": "oral",
+//       "substance_name": "Omeprazole"
+//     },
+//     {
+//       "brand_name": "Lasix",
+//       "dosage_form": "tablet",
+//       "generic_name": "Furosemide",
+//       "route": "oral",
+//       "substance_name": "Furosemide"
+//     },
+//     {
+//       "brand_name": "Zestril",
+//       "dosage_form": "tablet",
+//       "generic_name": "Lisinopril",
+//       "route": "oral",
+//       "substance_name": "Lisinopril"
+//     },
+//   ],
+//   "userName": {
+//     "firstName": "Alex",
+//     "lastName": "Thomas"
+//   }
+// }
 
 const API_KEY = "kLvHt2puR01mwrVfJM2yyP2m7CkQ1KvuIAAERSrc";
 const SEARCH_URL = "https://api.fda.gov/drug/label.json";
@@ -98,7 +98,7 @@ function generateSearchElement(item, itemIndex, template) {
   console.log(item.brand_name);
   return `<form class="js-add-form">
   <li class="js-item-index-element" data-item-index=${itemIndex}">
-    <span class="medication-item js-medication-item">Name: ${item.openfda.brand_name}</span>TEST<br>
+    <span class="medication-item js-medication-item">Name: ${item.openfda.brand_name}</span><br>
     <span class="medication-item js-medication-item">Form: ${item.openfda.dosage_form}</span><br>
     <span class="medication-item js-medication-item">Generic: ${item.openfda.generic_name}</span><br>
     <span class="medication-item js-medication-item">Route: ${item.openfda.route}</span><br>
@@ -109,12 +109,11 @@ function generateSearchElement(item, itemIndex, template) {
       <input type="text" id="user-frequency" class="user-entry"/>Frequency<br>
       <input type="text" id="user-dosage" class="user-entry"/>Dosage<br>
       <input type="hidden" id="route" value="${item.openfda.route}">
-        <input type="hidden" id="brand-name" value="${item.openfda.brand_name}">
-          <input type="hidden" id="generic-name" value="${item.openfda.generic_name}">
-            <input type="hidden" id="dosage-form" value="${item.openfda.dosage_form}">
-              <input type="hidden" id="substance-name" value="${item.openfda.substance_name}">
-  </li>
-  </form>`;
+      <input type="hidden" id="brand-name" value="${item.openfda.brand_name}">
+      <input type="hidden" id="generic-name" value="${item.openfda.generic_name}">
+      <input type="hidden" id="dosage-form" value="${item.openfda.dosage_form}">
+      <input type="hidden" id="substance-name" value="${item.openfda.substance_name}"></li>
+      </form>`;
 }
 
 function generateMedicationString(medications) {
